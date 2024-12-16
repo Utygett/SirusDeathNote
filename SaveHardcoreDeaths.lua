5f0b1829b5fc90b9ee68f35f374f6cef4af2d5a0
nikolai <kapustin.nikolai@vniizt.ru>
2024-12-16 16:51:30 +0300

    Очистка кода

--- SaveHardcoreDeaths.lua
+++ SaveHardcoreDeaths.lua
@@ -35,28 +35,6 @@ function ShowDeathMessage(textMessage)
     end)
 end
 
-
-
-
-
-
-
--- local classColor ={
---     ["Hunter"] = { r = 0.67, g = 0.83, b = 0.45, a=1 },
---     ["Warlock"] = { r = 0.58, g = 0.51, b = 0.79, a=1 },
---     ["Priest"] = { r = 1.0, g = 1.0, b = 1.0, a=1 },
---     ["Paladin"] = { r = 0.96, g = 0.55, b = 0.73, a=1 },
---     ["Mage"] = { r = 0.41, g = 0.8, b = 0.94, a=1 },
---     ["Rogue"] = { r = 1.0, g = 0.96, b = 0.41, a=1 },
---     ["Druid"] = { r = 1.0, g = 0.49, b = 0.04, a=1 },
---     ["Shaman"] = { r = 0.14, g = 0.35, b = 1.0, a=1 },
---     ["Warrior"] = { r = 0.78, g = 0.61, b = 0.43, a=1 },
---     ["DeathKnight"] = { r = 0.77, g = 0.12, b = 0.23, a=1 },
---     ["Evoker"] = { r = 0.7, g = 0.1, b = 0.1, a=1 },
---     ["DemonHunter"] = { r = 0.63, g = 0.58, b = 0.24, a=1 }
--- }
-
-
 -- Таблица для хранения объектов событий
 local deathList = {}
 local parsedDeathList = {}
@@ -89,31 +67,9 @@ local function SaveEvents()
 end
 
 
-
-
--- local function SendSendAllRecordsToSender(targetPlayer)
---     for _, record in ipairs(deathList) do
---         local serialized = SerializeRecord(record)
---         SendMessageToPlayerOnSameServer("MyAddon", serialized, "WHISPER", targetPlayer)
---     end
--- end
-
--- local function AddRecordToMyList(record)
---     print("AddRecordToMyList: ", record)
---     local death =  Death:ParseHardcoreDeath(record)
--- end
-
-
--- Обработчик событий
--- deathEventFrame:
-
--- Инициализация таблицы
-
 FrameUi = UiMainFramne:new(parsedDeathList)
--- Фрейм для обработки событий
--- local deathEventFrame = CreateFrame("Frame")
 
--- Событие, которое срабатывает при входе в игру
+-- Регистрация обрабатываемых событий
 FrameUi.frame:RegisterEvent("PLAYER_LOGIN")
 FrameUi.frame:RegisterEvent("PLAYER_LOGOUT")
 FrameUi.frame:RegisterEvent("CHAT_MSG_ADDON")
@@ -123,33 +79,14 @@ FrameUi.frame:SetScript("OnEvent", function(_, event, prefix, message)
         print("Аддон SaveHardcoreDeaths загружен! Используйте команды: /shd show")
         LoadSavedEvents()
         FrameUi.InitSettings(FrameUi)
-        -- Пример использования функции
-        -- FrameUi.frame:Show();
-        -- print("Check UserSettings.dateTimeForSynch: ", UserSettings.dateTimeForSynch)
-        
-        -- print("Check UserSettings.SyncWithGuild: ", UserSettings.SyncWithGuild)
-        -- print("Check UserSettings.SyncWithFriends: ", UserSettings.SyncWithFriends)
-        -- Регистрируем префикс
-        -- RegisterPrefix("MyAddon")
-        -- Пример использования
-        -- SendMessageToPlayerOnSameServer("MyAddon", "Hello!", "WHISPER", "Ещёпятьминут")
-        -- SendMessageToPlayerOnSameServer("MyAddon", "GET_ALL_DEATH_RECORDS@Нумампожал", "WHISPER", "Ещёпятьминут")
     elseif event == "PLAYER_LOGOUT" then
         SaveEvents()
     elseif event == "CHAT_MSG_ADDON" and prefix == "ASMSG_HARDCORE_DEATH" then
-        -- print("--------CHAT_MSG_ADDON: ", message)
         local death, parsedDeath = Death:new(message, date("%Y-%m-%d %H:%M:%S"))
         if death ~= nil then
             AddToMap(DeathListSaved, death)
             table.insert(parsedDeathList, parsedDeath)    
         end
-        
-        -- Пример использования функции 
-        -- ShowDeathMessage(death.GetDescription(death))
-        -- print("Событие добавлено: " .. event:GetDescription())
-        -- local deathSerealize = SerializeRecord(event)
-        -- print("-------------------Serealized death:", deathSerealize)
-        -- AddToMap(DeathListSaved, deathSerealize);
     elseif event == "CHAT_MSG_ADDON"  and prefix == "MyAddon" then      -- ///вынести в отдельную функцию
         -- print("Аддон ивент: ", event)
         -- print("Аддон префикс: ", prefix)
